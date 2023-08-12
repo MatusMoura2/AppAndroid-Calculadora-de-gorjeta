9b074e8d35e3785106e868360a35b4c35b92ebc3
MatusMoura2 <matus.moura333x2@gmail.com>
2023-08-12 13:53:32 -0300

    iniciando codigos java implementando propiedades

--- app/src/main/java/com/matusmoura/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/matusmoura/calculadoradegorjeta/MainActivity.java
@@ -3,12 +3,27 @@ package com.matusmoura.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editValue;
+    private TextView textTip;
+    private TextView textPercentage;
+    private TextView textTotal;
+    private SeekBar seekBarTip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValue       = findViewById(R.id.ImputValue);
+        textTip         = findViewById(R.id.textTip);
+        textPercentage  = findViewById(R.id.idPercentage);
+        textTotal       = findViewById(R.id.textTotal);
+        seekBarTip      = findViewById(R.id.seekBarTip);
     }
 }
