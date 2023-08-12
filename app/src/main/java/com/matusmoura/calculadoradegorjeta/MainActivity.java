73948752dcb0b02375c1bc168c738d0027a3ee46
MatusMoura2 <matus.moura333x2@gmail.com>
2023-08-12 18:43:01 -0300

    implementando metodo seekbar

--- app/src/main/java/com/matusmoura/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/matusmoura/calculadoradegorjeta/MainActivity.java
@@ -10,10 +10,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText editValue;
-    private TextView textTip;
+    private TextView valueTip;
     private TextView textPercentage;
     private TextView textTotal;
     private SeekBar seekBarTip;
+
+    private double percentage = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +24,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editValue       = findViewById(R.id.ImputValue);
-        textTip         = findViewById(R.id.textTip);
+        valueTip         = findViewById(R.id.valueTip);
         textPercentage  = findViewById(R.id.idPercentage);
         textTotal       = findViewById(R.id.textTotal);
         seekBarTip      = findViewById(R.id.seekBarTip);
+
+
+        //Manipulando SeekBar
+
+        seekBarTip.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                percentage = progress;
+                textPercentage.setText(percentage + " %");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        }
     }
-}
