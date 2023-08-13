a8f99b3840eb0da082356c63f56a6e59585c1cf4
MatusMoura2 <matus.moura333x2@gmail.com>
2023-08-13 01:06:26 -0300

    implementando calculo de porcentagem

--- app/src/main/java/com/matusmoura/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/matusmoura/calculadoradegorjeta/MainActivity.java
@@ -1,11 +1,14 @@
 package com.matusmoura.calculadoradegorjeta;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         editValue       = findViewById(R.id.ImputValue);
-        valueTip         = findViewById(R.id.valueTip);
+        valueTip        = findViewById(R.id.valueTip);
         textPercentage  = findViewById(R.id.idPercentage);
         textTotal       = findViewById(R.id.textTotal);
         seekBarTip      = findViewById(R.id.seekBarTip);
@@ -36,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 percentage = progress;
-                textPercentage.setText(percentage + " %");
+                textPercentage.setText(Math.round(percentage) + " %");
+                calcular();
             }
 
             @Override
@@ -50,5 +55,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        }
+        public void calcular(){
+         String valuePrimary = editValue.getText().toString();
+         if (valuePrimary == null || valuePrimary.equals(" ")){
+             Toast.makeText(getApplicationContext(),"Digite um valor!",Toast.LENGTH_LONG).show();
+         }else{
+
+             double valueImput = Double.parseDouble(valuePrimary);
+
+             double tip = valueImput * (percentage/100);
+
+             valueTip.setText("R$ "+Math.round(tip));
+         }
         }
     }
